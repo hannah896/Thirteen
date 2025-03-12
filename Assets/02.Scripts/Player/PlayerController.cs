21e2da9912456dfed1cf1e97a5f496eee13d18cd
park8306 <kosaf0804@gmail.com>
2025-03-12 19:51:05 +0900

    ✨ Feat: "간단한 캐릭터 이동 구현"

---
+++ Assets/02.Scripts/Player/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody rigid;        // 캐릭터의 리지드 바디
+
+    private Vector2 inputDir;       // 입력받은 방향
+
+    private void Start()
+    {
+        rigid = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        rigid.velocity = new Vector3(inputDir.x, rigid.velocity.y, inputDir.y);
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        inputDir = context.ReadValue<Vector2>();
+    }
+}
