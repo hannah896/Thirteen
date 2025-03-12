151371c08f3fbaffdf18345f8ad7b604ab134e75
hannah896 <phn2260@naver.com>
2025-03-13 02:33:11 +0900

    ✨ Feat: “아이템 스크립트 추가”
    구조 쉽지않네요

---
+++ Assets/02.Scripts/Item/ScriptableObject/ItemData.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VInspector;
+public enum ItemType
+{
+    Equipable,
+    Consumable,
+    Resource
+}
+
+public enum ConsumableType
+{
+    Health,
+    Hunger,
+    Thirsty,
+    Stemina
+}
+
+[CreateAssetMenu (fileName = "ItemBase", menuName = "New Item")]
+public class ItemData : ScriptableObject
+{
+    [Header("Info")]
+    public string itemName;
+    public string itemDescription;
+    public Sprite Icon;
+
+    [Header("Stacking")]
+    public bool canStack;
+    public int maxStackAmount;
+
+    [Header("Effect"), ShowInInspector]
+    public Dictionary<ConsumableType, float> Effects;
+
+    [Header("Equip")]
+    public GameObject equipPrefab;
+}
