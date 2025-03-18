dacc5f254b6dac8c128e92cd920eb9325cc6ad87
Jujeongmin <anjshdkdl99@gmail.com>
2025-03-18 12:59:28 +0900

    🎨 Style: “안쓰는 코드 제거”

--- Assets/02.Scripts/UI/TempZone.cs
+++ Assets/02.Scripts/UI/TempZone.cs
@@ -6,11 +6,6 @@ public class TempZone : MonoBehaviour
     [SerializeField]
     private BodyTemp bodyTemp;
 
-    //public int damage;
-    //public float damageRate;
-
-    //List<IDamageable> things = new List<IDamageable>();
-
     public enum ZoneType
     {
         Hot,
@@ -19,20 +14,7 @@ public class TempZone : MonoBehaviour
 
     [SerializeField]
     private ZoneType zoneType;
-
-    // 불이나 얼음 데였을 때 바로 데미지 입게 하고 싶으면 활성화
-    //private void Start()
-    //{
-    //    InvokeRepeating("DealDamage", 0, damageRate);
-    //}
-
-    //void DealDamage()
-    //{
-    //    for (int i = 0; i < things.Count; i++)
-    //    {
-    //        things[i].TakeDamage(damage);
-    //    }
-    //}
+ 
 
     private void OnTriggerStay(Collider other)
     {
@@ -56,22 +38,4 @@ public class TempZone : MonoBehaviour
             bodyTemp.ExitTempZone();
         }
     }
-
-    // 불이나 얼음 데였을 때 바로 데미지 입게 하고 싶으면 활성화
-    //private void OnTriggerEnter(Collider other)
-    //{
-    //    if(other.TryGetComponent(out IDamageable damageable))
-    //    {
-    //        things.Add(damageable);
-    //    }
-    //}
-
-    // 불이나 얼음 데였을 때 바로 데미지 입게 하고 싶으면 활성화
-    //private void OnTriggerExit(Collider other)
-    //{
-    //    if(other.TryGetComponent(out IDamageable damageable))
-    //    {
-    //        things.Remove(damageable);
-    //    }
-    //}
 }
